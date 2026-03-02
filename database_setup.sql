a4ee24b7ac0a172982b86b7ff4f66b55cb83946f
sikkunt77 <sikkunt77@gmail.com>
2026-03-02 13:24:55 +0700

    Update database_setup.sql

--- database_setup.sql
+++ database_setup.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `authors`;
 CREATE TABLE `authors` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`author_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 

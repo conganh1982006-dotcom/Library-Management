70449df8e67b798c09192eaa315a8462a21b49d8
Pham Cong Anh <conganh1982006@gmail.com>
2026-04-08 00:52:01 +0700

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // Chỉ làm đúng 1 nhiệm vụ: Kích hoạt cửa sổ Đăng Nhập
         LoginPage.main(null);
     }
 }
